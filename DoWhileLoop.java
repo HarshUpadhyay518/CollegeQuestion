856e8b2ef29dfd6e40c3c5d82e92a96c820f024d
Harsh Upadhyay <126193929+HarshUpadhyay518@users.noreply.github.com>
2024-12-24 11:26:58 +0530

    Create DoWhileLoop.java

---
+++ DoWhileLoop.java
@@ -0,0 +1,15 @@
+//---QUESTION-1: PRINT THE SUM OF THE STREAM OF INTEGERS IN THE INPUT--//
+// import java.util.*;
+// public class DoWhileLoop {
+//    public static void main(String[] args) {
+//     Scanner sc=new Scanner(System.in);
+//     System.out.println("Enter the number :");
+//     int n=sc.nextInt();
+//     int sum=0;
+//     do{
+//         sum+=n;
+//         n=sc.nextInt();
+//     }while(n!=-1);
+//     System.out.println(sum);
+//    } 
+// }
